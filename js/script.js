095c45b0f7d1709a6e0440340fdc1c59cec3d472
Alejandro <alejandro.quiros@live.u-tad.com>
2017-02-22 13:58:51 +0100

    completo json

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "../url.json";
+var url = "data/url.json";
 
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myArr = JSON.parse(this.responseText);
-        myFunction(myArr);
+        Jsonn(myArr);
+
     }
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
-function Json(arr) {
+function Jsonn(arr) {
     var out = "";
     var i;
-    for(i = 0; i < arr.length; i++) {
-        out += '<a class="linkbox" href="' + arr[i].url + '">' +
-        arr[i].url + '</a>';
+    
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i].priority == "true") {
+            out += '<a class="linkbox fixedpost" href="' + arr[i].url + '">' +
+        arr[i].display + '</a>';
+        }
+        if (arr[i].priority == "false") {
+            out += '<a class="linkbox" href="' + arr[i].url + '">' +
+            arr[i].display + '</a>';
+        }
+        
     }
     document.getElementById("id01").innerHTML = out;
 }
